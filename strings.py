b7cd1ae8e25c897ef0d70e02fefe3e7315a84373
Dylanm1995 <dylanmartin555@gmail.com>
2020-05-14 13:37:36 +0100

    variables and types

--- strings.py
+++ strings.py
@@ -5,5 +5,16 @@ print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "hello"
 name = input("Please enter your name ")
+
 # if we want a space we can add that too
 print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
