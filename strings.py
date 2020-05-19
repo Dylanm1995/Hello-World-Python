db6bcf71a4c965556fc857c71b6c4a9434dc84b8
Dylanm1995 <dylanmartin555@gmail.com>
2020-05-19 10:50:34 +0100

    f-strings commit

--- strings.py
+++ strings.py
@@ -15,6 +15,16 @@ print(age)
 print(type(greeting))
 print(type(age))
 
-age = "2 years"
+age = 24
 print(age)
 print(type(age))
+
+age_in_words = "2 years"
+print(name + f" is {age} years old")
+print(type(age))
+
+print()
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
